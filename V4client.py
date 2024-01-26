7dd49dd88868530bfd51204c227dbb9deb583373
Davidecasaro <davide.casaro@alessandrinimainardi.edu.it>
2024-01-26 09:21:41 +0100

    V4 finished

--- V4client.py
+++ V4client.py
@@ -1,11 +1,40 @@
-import socket,sys,random,os,time,threading,multiprocessing
-
+import socket
+import json
+from random import *
+import sys
+import os
+import time
+import threading
+import multiprocessing
+ 
 SERVER_ADDRESS='127.0.0.1'
 SERVER_PORT=22224
 NUM_WORKERS=15
+BUFFER_SIZE=1024
 
-def genera_richieste(address,port):
-    print(f"{threading.current_thread().name} execution time=", end_time_thread - start_time_thread)
+def genera_richieste(SERVER_ADDRESS,SERVER_PORT):
+    start_time_thread=time.time()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock_service:
+        sock_service.connect((SERVER_ADDRESS,SERVER_PORT))
+        primoNumero=randint(1,10)
+        secondoNumero=randint(1,10)
+        ind=randint(0,4)
+        vet=['+','-','*','/','%']
+        operazione=vet[ind]
+
+        message={
+            'primoNumero':primoNumero,
+            'operazione':operazione,
+            'secondoNumero':secondoNumero
+            }
+        
+        message=json.dumps(message) 
+        sock_service.sendall(message.encode("UTF-8"))
+        data=sock_service.recv(BUFFER_SIZE)
+        print(f"risultato: ",data.decode())
+
+        end_time_thread=time.time()
+        print(f"{threading.current_thread().name} execution time=", end_time_thread-start_time_thread)
 
 
 if __name__=='__main__':
@@ -15,4 +44,4 @@ if __name__=='__main__':
     [thread.join() for thread in threads]
     end_time=time.time()
 
-    print("total threads time=", end_time - start_time)
+    print("total threads time=", end_time-start_time)
